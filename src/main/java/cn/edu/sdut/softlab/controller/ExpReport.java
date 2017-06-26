bfb6f69e834d414b69533e383fec1cfc452dc4fd
GaoYisheng <1160139211@qq.com>
2017-06-26 11:58:19 +0800

    将文件名更改为 类名, 文件名由HandInService来组织,也为了以后扩展成其他语言 .c .cpp

--- src/main/java/cn/edu/sdut/softlab/controller/ExpReport.java
+++ src/main/java/cn/edu/sdut/softlab/controller/ExpReport.java
@@ -16,10 +16,16 @@
  */
 package cn.edu.sdut.softlab.controller;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.context.SessionScoped;
 import javax.enterprise.inject.Default;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedProperty;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import cn.edu.sdut.softlab.entity.*;
@@ -31,18 +37,25 @@ import cn.edu.sdut.softlab.entity.*;
 @Named("expReport")
 @Default
 public class ExpReport {
-	private String fileName;//文件名
-	private String result;//返回值
-	private String filePath;//文件保存的路径
-	private String answerText;//代码(答案)
-	
-	private ItemBank question;//当前题目
-	
-	//注入受管bean
-	@SessionScoped
-	@ManagedProperty(value ="#{login.currentUser}" )
-	private Student currentUser ;//当前用户
+	@Inject
+	FacesContext facesContext;
 
+	@Inject
+	private transient Logger logger;
+
+	private String className;// 文件名
+	private String result;// 返回值
+	private String filePath;// 文件保存的路径
+	private String answerText;// 代码(答案)
+
+
+
+	private ItemBank question;// 当前题目
+
+	// 注入受管bean
+	@SessionScoped
+	@ManagedProperty(value = "#{login.currentUser}")
+	private Student currentUser;// 当前用户
 
 	public ItemBank getQuestion() {
 		return question;
@@ -52,12 +65,25 @@ public class ExpReport {
 		this.question = question;
 	}
 
-	public String getFileName() {
-		return fileName;
+	public String getClassName() {
+		// 临时变量
+		String classStr = answerText.substring(answerText.indexOf("public class"), answerText.indexOf("{")).toString();// 获取类名字符串
+		String[] classStrArray = classStr.split("\\s{1,}");// 按空格分开
+		if (classStrArray.length != 3) {
+			//req.setAttribute("msg", "编译失败：格式不符合规范，请检查类名是否正确(如：public class YouClassName{})");
+
+			facesContext.addMessage(null, new FacesMessage("编译失败：格式不符合规范，请检查类名是否正确 !(如：public class ClassName{}) 错误来源=>" + classStrArray.toString()));
+			
+		} else {
+			className = classStrArray[classStrArray.length - 1];
+
+		}
+
+		return className;
 	}
 
-	public void setFileName(String fileName) {
-		this.fileName = fileName;
+	public void setClassName(String className) {
+		this.className = className;
 	}
 
 	public String getAnswerText() {
